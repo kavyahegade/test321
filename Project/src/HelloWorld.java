e987ccea139a11d5f13a9dc3d66938c6f24d00a6
kavyahegade <kavyashreeubdt@gmail.com>
2020-09-29 17:05:05 +0530

    Adding hello world.

---
+++ Project/src/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author CHETANA J N
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
